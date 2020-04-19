9e01a4167a511a62d94d5b054f0b24bbb75b1f59
Alkuna <arif.dharmawan05@gmail.com>
2020-04-19 17:24:23 +0700

    merevisi

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function draw() {
     //1
     var panjangPP1 = 100;
     var tinggiPP1 = 150;
+    
     contex.fillStyle='red';
     contex.fillRect(10, 350, panjangPP1, tinggiPP1);
     
     //tutup
     var panjangPP2 = 100;
     var tinggiPP2 = 30;
+
     contex.fillStyle='gray';
     contex.fillRect(10, 320, panjangPP2, tinggiPP2);
 
@@ -21,7 +23,6 @@ function draw() {
     var tinggiPP3 = 18;
     contex.fillStyle='black';
     contex.fillRect(47, 302, panjangPP3, tinggiPP3);
-
 
     contex.fillStyle='red';
     contex.fillRect(350, 350, panjangPP1, tinggiPP1);
