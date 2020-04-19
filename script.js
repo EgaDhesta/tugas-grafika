e61c00d50ba23674595de8f59fd18fb2aad7dda8
Alkuna <arif.dharmawan05@gmail.com>
2020-04-19 16:20:16 +0700

    menambah script

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ var contex = myCanvas.getContext('2d');
 myCanvas.width=myCanvas.scrollWidth;
 myCanvas.height=myCanvas.scrollHeight;
 
-// jwonfeownefonWO
-// KCADMD
-// isisbdausdvuadb
+function draw() {
+    //1
+    var panjangPP1 = 100;
+    var tinggiPP1 = 150;
+    contex.fillStyle='red';
+    contex.fillRect(10, 350, panjangPP1, tinggiPP1);
+    
+    //tutup
+    var panjangPP2 = 100;
+    var tinggiPP2 = 30;
+    contex.fillStyle='gray';
+    contex.fillRect(10, 320, panjangPP2, tinggiPP2);
+}
