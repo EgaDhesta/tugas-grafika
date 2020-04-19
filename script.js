fb1ff0d3d4fca3d2815eb375690841fbd9c3f241
egadhesta <egadhesta@gmail.com>
2020-04-19 16:08:28 +0700

    menambah coment js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ var myCanvas = document.getElementById('canvas')
 var contex = myCanvas.getContext('2d');
 myCanvas.width=myCanvas.scrollWidth;
 myCanvas.height=myCanvas.scrollHeight;
+
+// jwonfeownefonWO
+// KCADMD
