13f0eb55ebfe4c4f9d0de8ee7f9b466d1256c31d
Alkuna <arif.dharmawan05@gmail.com>
2020-04-19 17:18:47 +0700

    menambahkan script

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function draw() {
     var tinggiPP2 = 30;
     contex.fillStyle='gray';
     contex.fillRect(10, 320, panjangPP2, tinggiPP2);
-}
+
+    //pegangan
+    var panjangPP3 = 23;
+    var tinggiPP3 = 18;
+    contex.fillStyle='black';
+    contex.fillRect(47, 302, panjangPP3, tinggiPP3);
+
+
+    contex.fillStyle='red';
+    contex.fillRect(350, 350, panjangPP1, tinggiPP1);
+    }
